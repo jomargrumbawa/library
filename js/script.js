7e37ffeb63b586dc16ff4dd18098d870b7b1a22d
Jomar Rumbawa <jomargrumbawa@gmail.com>
2024-08-15 09:29:51 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+const bookFormDialog = document.getElementById('book-form-dialog');
+const openDialogBtn = document.getElementById('open-dialog-btn');
+const closeDialogBtn = document.getElementById('close-dialog-btn');
+const submitBtn = document.getElementById('submit-btn');
+const bookForm = document.getElementById('book-form');
+
 let myLibrary = [{
   title: "Book 1",
   pages: 200,
@@ -17,6 +23,25 @@ let myLibrary = [{
   isRead: true
 }];
 
+openDialogBtn.addEventListener('click', () => {
+  bookFormDialog.showModal();
+})
+
+closeDialogBtn.addEventListener('click', () => {
+  bookFormDialog.close();
+})
+
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const isRead = document.getElementById('is-read').checked;
+  myLibrary.push(new Book(title, pages, author, isRead));
+  bookFormDialog.close();
+  bookForm.reset();
+  displayBooks();
+})
 
 function Book(title, pages, author, isRead) {
   this.title = title;
@@ -30,6 +55,7 @@ function addBookToLibrary(book) {
 }
 
 function displayBooks() {
+  document.getElementById('books').innerHTML = '';
   for (let i = 0; i < myLibrary.length; i++) {
     let book = myLibrary[i];
     let newBook = document.createElement('div');
@@ -46,11 +72,18 @@ function displayBooks() {
     let isRead = document.createElement('p');
     isRead.textContent = book.isRead ? 'Read' : 'Not Read';
 
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.addEventListener('click', () => {
+      myLibrary.splice(i, 1);
+      displayBooks();
+    })
+
     newBook.appendChild(title);
     newBook.appendChild(author);
     newBook.appendChild(pages);
     newBook.appendChild(isRead);
-
+    newBook.appendChild(deleteBtn);
     document.getElementById('books').appendChild(newBook);
   }
 }
